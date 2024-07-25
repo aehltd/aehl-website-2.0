913fd5102c75e2b61c121e3e2d62b10cfcf0d85f
Jelynfish <thepixeltomato@gmail.com>
2024-07-25 15:16:36 -0400

    Removed PDF from assets

--- src/components/IRComponents/Presentations.tsx
+++ src/components/IRComponents/Presentations.tsx
@@ -3,13 +3,15 @@ import React from "react";
 const PresentationsRow = () => {
   return (
     <div>
-      <embed
-        src={require("../../assets/AEHL Company Presentation June 2024.pdf")}
-        type="application/pdf"
+      <iframe
+        src="https://www.dropbox.com/scl/fi/k14ych2noa7885kidwd9s/AEHL-Company-Presentation-June-2024.pdf?rlkey=xxfjf7hw738ne3n2ut5qob78v&st=j74xnfm7&raw=1"
         width="100%"
         height="600px"
-        className="mt-4"
+        title="AEHL Company Presentation June 2024"
+        className="border rounded shadow-lg mt-4"
       />
+      {/* SQUARESPACE LINK */}
+      {/* https://static1.squarespace.com/static/65f5178b7e5b446365b2d70a/t/66a29ca50148a07cea85a615/1721932966168/AEHL+Company+Presentation+June+2024.pdf */}
     </div>
   );
 };
