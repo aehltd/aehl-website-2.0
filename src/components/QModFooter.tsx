d3a976d048d871ddf88489a3ea2fcba0e5a9c00d
Jelynfish <thepixeltomato@gmail.com>
2024-07-29 13:49:26 -0400

    Removed testing from homescreen.

---
+++ src/components/QModFooter.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+
+const QModFooter = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "qmod";
+    script.type = "application/javascript";
+    script.src = "https://qmod.quotemedia.com/js/qmodLoader.js";
+    script.setAttribute("data-qmod-wmid", "101150");
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="qmod-attribution">
+      <a href="https://www.quotemedia.com/coverage" className="qmf-link">
+        Financial Market Data
+      </a>
+      copyright © 2020
+      <a href="https://www.quotemedia.com" className="qmf-link">
+        QuoteMedia
+      </a>
+      . Data delayed 15 minutes unless otherwise indicated (view
+      <a
+        href="https://www.quotemedia.com/legal/tos/#times"
+        className="qmf-link"
+      >
+        delay times
+      </a>
+      for all exchanges). <span className="qmf-rt">RT</span>=Real-Time,
+      <span className="qmf-non-rt">EOD</span>=End of Day,
+      <span className="qmf-non-rt">PD</span>=Previous Day.
+      <a href="https://www.quotemedia.com/coverage" className="qmf-link">
+        Market Data
+      </a>
+      powered by
+      <a href="https://www.quotemedia.com" className="qmf-link">
+        QuoteMedia
+      </a>
+      . Analyst Ratings by Zacks.
+      <a href="https://www.quotemedia.com/legal/tos/" className="qmf-link">
+        Terms of Use
+      </a>
+      .
+      <div className="qmf-logo">
+        <a href="https://www.quotemedia.com">
+          <img
+            src="//www.quotemedia.com/footer/logo.png"
+            alt="QuoteMedia logo"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/components/QModWidget.tsx
+++ src/components/QModWidget.tsx
@@ -1,56 +1,15 @@
 import React, { useEffect } from 'react';
 
 interface QModComponentProps {
-    tool: string;
+    tool: string,
+    params: string,
 }
 
-const QModComponent: React.FC<QModComponentProps> = ({tool}) => {
-  useEffect(() => {
-    // Load the QMod stylesheet
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '/wp-content/themes/generatepress/css/qmod-style.css';
-    document.head.appendChild(link);
-
-    // Load the QMod script
-    const script = document.createElement('script');
-    script.id = 'qmod';
-    script.type = 'application/javascript';
-    script.src = 'https://qmod.quotemedia.com/js/qmodLoader.js';
-    script.setAttribute('data-qmod-wmid', '101150');
-    document.body.appendChild(script);
-
-    return () => {
-      // Clean up the added elements if needed
-      document.head.removeChild(link);
-      document.body.removeChild(script);
-    };
-  }, []);
-
+const QModComponent: React.FC<QModComponentProps> = ({tool, params}) => {
   return (
     <div
       data-qmod-tool={tool}
-      data-qmod-params={`{
-        "lang":"en",
-        "socialshare":true,
-        "resultsPerPage":10,
-        "excludeTopics":"NONCOMPANY",
-        "noSrc":"qmr",
-        "summLen":100,
-        "showLogo":false,
-        "lowHigh":false,
-        "videonews":true,
-        "showVideos":true,
-        "showSharingButtons":true,
-        "showThumbnail":false,
-        "dateHeadings":true,
-        "yearSelect":false,
-        "yearSelectNumYears":2,
-        "yearSelected":0,
-        "datePickerFormat":"Y-m-d",
-        "symbol":"AEHL"
-      }`}
+      data-qmod-params={params}
       className="qtool"
     ></div>
   );
