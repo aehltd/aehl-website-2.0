3f19e61133efdb3efff136b67d690f3540d98db8
Jelynfish <thepixeltomato@gmail.com>
2024-07-29 15:27:26 -0400

    fix a warning

--- src/pages/ir/EmailAlertsPage.tsx
+++ src/pages/ir/EmailAlertsPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Checkbox, Input } from "antd";
+//import { Button, Checkbox, Input } from "antd";
 import QModComponent from "../../components/QModWidget";
 import QModFooter from "../../components/QModFooter";
 
