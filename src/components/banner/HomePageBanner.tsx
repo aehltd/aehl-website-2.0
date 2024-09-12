c3f9d9fa66dd82bc7a6cb879b95fc6783b8266a1
biubiubiupiu21 <94020092+timyao21@users.noreply.github.com>
2024-09-12 12:24:25 -0400

    bug fix

--- src/components/banner/HomePageBanner.tsx
+++ src/components/banner/HomePageBanner.tsx
@@ -2,13 +2,15 @@ export default function HomePageBanner() {
   return (
     <div
       className="relative flex flex-col bg-midnight justify-center items-center text-center h-[65vh] bg-center bg-cover w-full "
-      style={{
-        // backgroundImage:
-        //   "url(https://res.cloudinary.com/aehl/image/upload/v1724428460/3D_dohdwm.jpg)",
-      }}
+      style={
+        {
+          // backgroundImage:
+          //   "url(https://res.cloudinary.com/aehl/image/upload/v1724428460/3D_dohdwm.jpg)",
+        }
+      }
     >
-            <img
-        className="Static w-2/3"
+      <img
+        className="Static md:h-[25vh] md:w-auto w-3/4 h-auto"
         src="https://res.cloudinary.com/aehl/image/upload/v1724428464/%E5%A4%A7%E7%99%BD%E8%89%B2.jpg_voicgv.png"
         alt="AEHL US"
       />
